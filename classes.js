5ca3ce02c981f25ac1baade9623f08b44ad701d9
suhail kandanur <ksuhail7@gmail.com>
2016-11-27 21:19:48 -0500

    tutorials

--- classes.js
+++ classes.js
@@ -13,15 +13,26 @@ var classes;
         }
         Object.defineProperty(Car.prototype, "engine", {
             get: function () { return this._engine; },
-            set: function (engine) {
-                this._engine = engine;
+            set: function (value) {
+                if (value === undefined)
+                    throw "please supply an engine";
+                this._engine = value;
             },
             enumerable: true,
             configurable: true
         });
+        Car.prototype.start = function () {
+            alert('Car engine started ' + this._engine.engineType);
+        };
         return Car;
     }());
     var engine = new Engine(2000, "v8");
     var car = new Car(engine);
     console.log(car.engine);
+    window.onload = function () {
+        var engine = new Engine(300, 'v8');
+        var car = new Car(engine);
+        alert(car.engine.engineType);
+        car.start();
+    };
 })(classes || (classes = {}));
